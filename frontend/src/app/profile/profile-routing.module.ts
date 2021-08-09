c0b8c336703fc9a59cc0665690a4ad0a21b6e14d
malvdev <sergey.malkov.dev@gmail.com>
2021-08-09 22:25:02 +0500

    feat(frontend-profile): import profile resolve to route

--- frontend/src/app/profile/profile-routing.module.ts
+++ frontend/src/app/profile/profile-routing.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from '@core/auth';
+
+import { ProfileResolverService } from './services/profile-resolver.service';
 import { ProfileComponent } from './profile.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+    resolve: {
+      user: ProfileResolverService,
+    },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
+  providers: [ProfileResolverService],
 })
 export class ProfileRoutingModule {}
