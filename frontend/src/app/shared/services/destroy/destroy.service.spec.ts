8eb504478b1a0ba6e87b0669d25937a684bf5794
malvdev <sergey.malkov.dev@gmail.com>
2021-08-09 18:30:38 +0500

    feat(frontend-shared): add service to destroy subscription

---
+++ frontend/src/app/shared/services/destroy/destroy.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DestroyService } from './destroy.service';
+
+describe('DestroyService', () => {
+  let service: DestroyService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DestroyService],
+    });
+    service = TestBed.inject(DestroyService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
