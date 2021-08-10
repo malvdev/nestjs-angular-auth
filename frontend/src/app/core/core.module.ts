b155f4af2720684e5999fa09bd83842110ac1b2e
malvdev <sergey.malkov.dev@gmail.com>
2021-08-10 13:34:48 +0500

    refactor(frontend): remove unused imports

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { environment } from '../../environments/environment';
 import { AuthModule } from './auth';
+import { ApiService } from './api';
+import { StoragesModule } from './storages';
+import { APP_CONFIG } from './tokens';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AuthModule.forRoot({
-      apiBase: environment.serverUrl,
-    }),
-  ],
+  imports: [CommonModule, StoragesModule, AuthModule],
+  providers: [ApiService, { provide: APP_CONFIG, useValue: environment }],
 })
 export class CoreModule {}
--- frontend/src/app/core/index.ts
+++ frontend/src/app/core/index.ts
@@ -1,3 +1,5 @@
+export * from './api';
 export * from './auth';
+export * from './tokens';
 export * from './storages';
 export * from './core.module';
